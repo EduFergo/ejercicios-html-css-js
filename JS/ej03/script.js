60876559568c8018ee001e54ae17246c95f86af3
EduFergo <efergo93@gmail.com>
2024-04-12 15:02:16 +0200

    Ejercicio 3 JS completado

---
+++ JS/ej03/script.js
@@ -0,0 +1,14 @@
+const now = document.querySelector(`.now`);
+
+function setTime(){
+    const date_now = new Date();
+    const hours = date_now.getHours();
+    const minutes = date_now.getMinutes();
+    const seconds =  date_now.getSeconds();
+
+    const time = `${hours}:${minutes}:${seconds}`;
+
+    now.innerHTML = time;
+}
+
+const updateTime= setInterval(setTime, 1000);
